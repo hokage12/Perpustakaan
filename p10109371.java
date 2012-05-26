ea5f7a2b75d2bfe93be0e092c9e035477eda8530
Frencius Leo Nardus <frenciusleonardus@yahoo.com>
2012-05-26 11:46:41 +0700

    Komit Pertama

---
+++ p10109371.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1;
+
+/**
+ *
+ * @author Frencius Leo Nardus
+ */
+public class P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       // TODO code application logic here
+         Futama futama=new Futama();
+        futama.setVisible(true);
+    }
+}
